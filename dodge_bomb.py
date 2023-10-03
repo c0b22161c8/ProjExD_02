7d6faf7d429923a856525382b6e05aaf500ff147
yoshimoto <c0b22161c8@edu.teu.ac.jp>
2023-10-03 14:46:07 +0900

    bomb3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,12 +6,17 @@ from random import randint
 WIDTH, HEIGHT = 1600, 900
 
 
+key_dct = {pg.K_UP:(0,-5), pg.K_DOWN:(0,5), pg.K_LEFT:(-5,0), pg.K_RIGHT:(5,0)}
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    k_rect = (kk_img.get_rect())
+    k_rect.center=(900, 400)
     draw_c = pg.Surface((20, 20))
     pg.draw.circle(draw_c, (255,0,0), (10,10), 10)
     draw_c.set_colorkey((0,0,0))
@@ -22,15 +27,23 @@ def main():
     y=randint(10, HEIGHT)
     c_rect.center=(x,y)
     vx = vy = 5
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, k_rect)
         screen.blit(draw_c, c_rect)
         c_rect.move_ip(vx,vy)
+        key_lst = pg.key.get_pressed()
+        total_move = [0,0]
+        for key, move in key_dct.items():
+            if key_lst[key]:
+                total_move[0] += move[0]
+                total_move[1] += move[1]
+        k_rect.move_ip(total_move)
         pg.display.update()
         tmr += 1
         clock.tick(50)
